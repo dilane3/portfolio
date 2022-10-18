29a5e231c6897ff4b8db8460b4dfd9c954d435bc
dilane3 <komboudilane125@gmail.com>
2022-10-18 19:44:11 +0100

    ADD: Adding loading page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import GoUp from '../../components/GoUp'
 import ModalCore from '../../components/modals/ModalCore'
 import ModalContext from '../../datamanager/context/modalContext'
@@ -11,19 +11,35 @@ import Projects from './components/Projects'
 import Skills from './components/Skills'
 import styles from './styles/home.module.css'
 import ReactGa from 'react-ga'
+import { LoaderPage } from './components/Loader'
 
 const Home = () => {
   const { open: modalOpen, closeModal } = useContext(ModalContext)
+  const [showLoader, setShowLoader] = useState(true)
 
   useEffect(() => {
     ReactGa.initialize('UA-234891814-1')
     ReactGa.pageview('/')
   }, [])
 
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      setShowLoader(false)
+    })
+
+    return () => {
+      window.removeEventListener('load', () => {
+        setShowLoader(false)
+      })
+    }
+  }, [])
+
   return (
     <section
       className={styles.container}
     >
+      <LoaderPage show={showLoader} />
+
       <Navbar />
       <MobileNavbar />
 
---
+++ src/pages/home/components/Loader.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import styles from '../styles/loader.module.css';
+
+const logoWithoutBorder = require("../../../assets/images/logo-without-border.png")
+const logoBorder = require("../../../assets/images/logo-border.png")
+
+export const LoaderPage = ({ show }) => {
+  const [maskLoader, setMaskLoader] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMaskLoader(true);
+    }, [800])
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [show])
+
+  return (
+    <div
+      className={`
+        ${styles.loaderContainer} 
+        ${!show ? styles.loaderHidden : ""}
+      `}
+      style={maskLoader ? { zIndex: -10} : {}}
+    >
+      <div
+        className={styles.logo}
+      >
+        <img src={logoBorder} width={100} height={100} className={styles.logoBorder} />
+        <img src={logoWithoutBorder} width={100} height={100} />
+      </div>
+    </div>
+  )
+}
